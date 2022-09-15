6786df2f4c1921f0d1e5f809783198a0db06d4f0
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-09-16 03:31:46 +0800

    Email Store basics

--- src/stores/email.js
+++ src/stores/email.js
@@ -1,13 +1,45 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 // interface Email {
+//     id: Number,
 //     isRead: Boolean,
 //     subject: String,
 //     body: String,
 //     category: String
 // }
-
-export const useEmailStore = defineStore('email', () => {
-  return { }
+export const useEmailStore = defineStore('emails', {
+  state: () => ({
+    /** @type {{ id: number, isRead: boolean, subject: string, body: string, category: string }[]} */
+    emails: [
+      {
+        id: 1,
+        isRead: false,
+        subject: 'Your 7-figure plan goes bye-bye at midnight',
+        body: 'Lorem ipsum is simply dummy text',
+        category: 'INBOX'
+      },
+      {
+        id: 2,
+        isRead: false,
+        subject: '[Weekend only] get this now',
+        body: 'Lorem ipsum is simply dummy text',
+        category: 'INBOX'
+      },
+      {
+        id: 3,
+        isRead: true,
+        subject: 'Uh oh your prescription',
+        body: 'Lorem ipsum is simply dummy text',
+        category: 'INBOX'
+      },
+    ],
+  }),
+  getters: {
+    inboxMails(state){
+      return state.emails.filter(email => email.category === 'INBOX')
+    },
+    archivedMails(state){
+      return state.emails.filter(email => email.category === 'ARCHIVED')
+    },
+  },
 })
