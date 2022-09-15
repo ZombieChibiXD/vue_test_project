a81be46c456dd12162d3ca192d620a8d42c4d49d
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-09-16 00:43:26 +0800

    Better prototype for the layout page

--- src/App2.vue
+++ src/App2.vue
@@ -4,22 +4,27 @@
   </script>
 
   <template>
-    <div class="layout">
-      <Sidebar>
-        <template #top>
-          <button>Top</button>
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-          </nav>
-        </template>
-        <template #bottom>
-          <button>bottom</button>
-        </template>
-      </Sidebar>
-      <div>
-        <RouterView />
-      </div>
+    <div>
+      <section class="layout">
+        <Sidebar>
+          <template #top>
+            <button>Top</button>
+            <nav>
+              <RouterLink to="/">Home</RouterLink>
+              <RouterLink to="/about">About</RouterLink>
+            </nav>
+          </template>
+          <template #bottom>
+            <button>bottom</button>
+          </template>
+        </Sidebar>
+        <main>
+          <RouterView />
+        </main>
+      </section>
+      <section>
+        <!-- Mail view goes here. -->
+      </section>
     </div>
 
   </template>
