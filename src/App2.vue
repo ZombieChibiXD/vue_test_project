c4e582502296166102db15727a5ff91832d3f403
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-09-16 00:41:01 +0800

    Prototype Layout file

---
+++ src/App2.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import Sidebar from './components/Sidebar.vue';
+  </script>
+
+  <template>
+    <div class="layout">
+      <Sidebar>
+        <template #top>
+          <button>Top</button>
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </template>
+        <template #bottom>
+          <button>bottom</button>
+        </template>
+      </Sidebar>
+      <div>
+        <RouterView />
+      </div>
+    </div>
+
+  </template>
+
+  <style scoped>
+  </style>
