f2661de752f4063acb9a3f501ed130afd51ae1a9
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2022-09-16 01:58:11 +0800

    Better inbox prototype

--- src/components/Inbox.vue
+++ src/components/Inbox.vue
@@ -1,13 +1,29 @@
-<script setup></script>
+<script setup>
+import ListItem from './Email/ListItem.vue';
+</script>
 
 <template>
-  <div class="sidebar">
-    <div class="sidebar_top">
-      <slot name="top"></slot>
-    </div>
-    <div class="sidebar_bottom">
-      <slot name="bottom"></slot>
-    </div>
+  <div class="inbox">
+    <header class="inbox_header">
+      <slot name="header"></slot>
+    </header>
+    <section>
+      <h3>
+        Email Selected (0)
+      </h3>
+    </section>
+    <section>
+      <input type="checkbox">
+      <button>Mark as Read (r)</button>
+      <button>Archive</button>
+    </section>
+    <section>
+      <ListItem subject="'Test Subject'" />
+      <ListItem subject="'Test Subject'" />
+      <ListItem subject="'Test Subject'" />
+      <ListItem subject="'Test Subject'" />
+      <ListItem subject="'Test Subject'" />
+    </section>
   </div>
 </template>
 
--- src/components/email/ListItem.vue
+++ src/components/email/ListItem.vue
@@ -1,22 +1,21 @@
 <script setup>
-    defineProps({
-        checked:{
-            type:Boolean,
-            default: false
-        },
-        subject: {
-            type: String,
-            required: true
-        },
-    })
-    </script>
+defineProps({
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+});
+</script>
 
-    <template>
-        <div class="mail_list_item">
-            <input class="mail_list_item_checked" type="checkbox" v-model="checked">
-            <h3 class="mail_list_item_subject">{{ subject }}</h3>
-        </div>
-    </template>
+<template>
+  <div class="mail_list_item">
+    <input class="mail_list_item_checked" type="checkbox" v-model="checked" />
+    <h3 class="mail_list_item_subject">{{ subject }}</h3>
+  </div>
+</template>
 
-    <style scoped>
-    </style>
+<style scoped></style>
